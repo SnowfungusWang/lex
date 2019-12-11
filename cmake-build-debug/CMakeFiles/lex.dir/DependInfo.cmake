b22428cbe06dc21d50d778c36f1e8ee6f4f3bef5
YukiKun <kingsnowmushroom@126.com>
2019-12-11 23:58:49 +0800

    add

---
+++ cmake-build-debug/CMakeFiles/lex.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mush/code/complier/lex_git/lex/anaREs.cpp" "/home/mush/code/complier/lex_git/lex/cmake-build-debug/CMakeFiles/lex.dir/anaREs.cpp.o"
+  "/home/mush/code/complier/lex_git/lex/main.cpp" "/home/mush/code/complier/lex_git/lex/cmake-build-debug/CMakeFiles/lex.dir/main.cpp.o"
+  "/home/mush/code/complier/lex_git/lex/re2NFA.cpp" "/home/mush/code/complier/lex_git/lex/cmake-build-debug/CMakeFiles/lex.dir/re2NFA.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lex.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lex.dir/anaREs.cpp.o"
+  "CMakeFiles/lex.dir/main.cpp.o"
+  "CMakeFiles/lex.dir/re2NFA.cpp.o"
+  "lex"
+  "lex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
